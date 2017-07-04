a5543145ea1256135e28119cec13607153d4d6f5
mwj8410 <mjackson@treehouseonline.com>
2017-07-03 23:08:54 -0500

    fix(host): correct reload issue

--- host.js
+++ host.js
@@ -5,15 +5,13 @@ let app = express();
 
 app.set('view engine', 'ejs');
 
-app.get(`/`, (req, res) => {
+app.get(`/app/*`, (req, res) => {
   return res.render(`./demo`);
 });
 
 // Host Static files from `./tmp`
 app.use('/demo', express.static('.tmp/'));
 
-app.listen(1337, () => {
-  console.log('Host > listen: listening on port: 1337.');
+app.listen(8080, () => {
+  console.log('Host > listen: listening on port: 8080.');
 });
-
-
