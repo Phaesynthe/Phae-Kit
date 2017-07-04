ecf19ac89784ab6c1717ab6190e88ca2e442a9fe
mwj8410 <mjackson@treehouseonline.com>
2017-07-03 23:09:56 -0500

    chore(demo): adjust routing for new host structure

--- ui/demo.jsx
+++ ui/demo.jsx
@@ -1,23 +1,31 @@
 // Framework Level
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory, Router, Route } from 'react-router';
+import { createHistory } from 'history';
+import { useRouterHistory, Router, Route } from 'react-router';
+
+const browserHistory = useRouterHistory(createHistory)({
+  basename: '/app'
+});
 
 // Views
-import CaptainsConsole from 'View/captainsConsole.jsx';
-import ComponentIndex from 'View/componentIndex.jsx'; // ToDo: For development. Remove!
-import Dashboard from 'View/dashboard.jsx';
-import SplashScreen from 'View/splashScreen.jsx';
+import ComponentIndex from 'View/componentIndex.jsx';
+import DemoIndex from 'View/demoIndex.jsx';
+import StroyPartials from 'View/storyPartials.jsx';
 
 // Styles
 import 'Style/global.scss';
 
+// import { useRouterHistory } from 'react-router';
+
+
+
+
 render((
   <Router onUpdate={ () => window.scrollTo(0, 0) } history={ browserHistory }>
-    <Route path='/' component={ SplashScreen } />
-    <Route path="/CaptainsConsole" component={ CaptainsConsole } />
-    <Route path='/Dashboard' component={ Dashboard } />
+    <Route path='/' component={ DemoIndex } />
     <Route path='/ComponentIndex' component={ ComponentIndex } />
+    <Route path='/StoryPartials' component={ StroyPartials } />
   </Router>
 ),
-document.querySelector('#application'))
+document.querySelector('#application'));
